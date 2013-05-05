476b7879c7d1239598cd62e8e06c7bdd66911076
Dmitry Grebeniuk <gdsfh1+bu@gmail.com>
2013-05-05 19:27:01 +0300

    .

--- iteratees.ml
+++ iteratees.ml
@@ -1033,7 +1033,7 @@ value enumpart_readchars
            k (EOF (some & ierr_of_merr e)) >>% fun (it, sl') ->
            IO.return (it, lazy sl', EP_None)
        | `Ok have_read ->
-           let () = fdbg "ep: Read buffer, size %i\n" have_read in
+           let () = fdbg "ep: Read buffer, size %i" have_read in
            let () = assert (have_read >= 0) in
            if have_read = 0
            then
